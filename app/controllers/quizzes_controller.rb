f5680da7aa5fc3b083e0c0bf14e588b6e8a33527
Jocelyn Triplett <jocelyn.triplett@gmail.com>
2017-10-03 15:43:16 -0400

    starting work on Question form

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,2 +1,24 @@
 class QuizzesController < ApplicationController
+
+  def new
+    @quiz = Quiz.new
+  end
+
+  def show
+    @quiz = Quiz.find(params[:id])
+  end
+
+  def create
+    @quiz = Quiz.new(quiz_params)
+    # @quiz.user = User.find_by(id: session[:user_id])
+    @quiz.save
+    redirect_to @quiz
+  end
+
+  private
+
+    def quiz_params
+      params.require(:quiz).permit(:id, :title, :correct_answer, :url)
+    end
+
 end
