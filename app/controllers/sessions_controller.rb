7a1885a2cfa958d60ae6651c4bd6cf9bb6336720
gingin77 <ginniehench@gmail.com>
2017-10-04 11:42:34 -0400

    removed contents of assets directory

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,17 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
+    else
+      render 'new'
+    end
   end
 
   def destroy
+    reset_session
+    render 'log_out'
   end
 end
